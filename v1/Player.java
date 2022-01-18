f34fa55d344c5b53053fc50fece316a75c367b2e
lnafiz <75277473+lnafiz@users.noreply.github.com>
2022-01-18 12:27:12 -0500

    Update Player.java

--- v1/Player.java
+++ v1/Player.java
@@ -1,2 +1,11 @@
 public class Player extends Gambler{
+  protected String name;
+  
+  public Player(String input){
+    super();
+    name = input;
+    
+  public String getName(){
+    return name;
+  }
 }
