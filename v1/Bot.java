dfaada94eba3be81adeece2d3d0369cf23ecdb88
Brian Li <bli31@stuy.edu>
2022-01-18 20:16:13 -0500

    v1

--- v1/Bot.java
+++ v1/Bot.java
@@ -1,20 +1,24 @@
-<<<<<<< HEAD
 import java.util.ArrayList;
 
 public class Bot extends Gambler{
-  public Bot(ArrayList deckOfCards, ArrayList valueOfCards){
-    super(deckOfCards, valueOfCards);
+  public Bot(ArrayList deckOfCards){
+    super(deckOfCards);
+    System.out.println(hand); // diag
+    System.out.println("inHand: " + inHand); // diag
   }
 
-  // public boolean nextMove(){
-  //   // overwritten
-  // }
+  public boolean nextMove(){
+    System.out.println("This player's hand is... " + showHand());
+    int rng = (int)(Math.random() * 3);
+    if (rng == 0 && hand.size() == 2){
+      return doubleDown();
+    }
+    else if (rng == 0 || rng == 1){
+      hit();
+    }
+    else{
+      return stand();
+    }
+    return false;
+  }
 }
-=======
-public class Bot extends Gambler{
-  
-  public Bot(String input){
-    super();
-
-}
->>>>>>> 99133b1579fe8a6a4f580fb023eb6a3675070f91
--- v1/Woo.java
+++ v1/Woo.java
@@ -4,18 +4,26 @@
 // 2022-01-14
 // time spent: 1.2 hrs
 
+// TO DO:
+// change visibility of stuff, add mutator methods.
+// move the stuff from main method to another file?
+// shuffle player order
+// add scores/balance
+// use dealer
+// HI-LO CARD COUNTING
+
 import java.io.*;
 import java.util.*;
 
 public class Woo{
   public static void main(String[] args){
     Deck deck = new Deck();
+    int numBots = 0;
     Gambler[] totalBots = new Bot[0];
-    Gambler humanPlayer = new Gambler(deck.cardsRemaining. deck.cardValuesRemaining);
     ArrayList totalPlayers = new ArrayList<Gambler>();
+    Gambler humanPlayer = new Player(deck.cardsRemaining, totalPlayers);
     System.out.println(deck.cardsRemaining); // diag
 
-    int numBots = 0;
     InputStreamReader isr = new InputStreamReader(System.in);
     BufferedReader in = new BufferedReader(isr);
 
@@ -33,7 +41,7 @@ public class Woo{
       System.out.println("Invalid number of bots! Playing alone.");
     }
     for (Gambler bot : totalBots){
-      bot = new Bot(deck.cardsRemaining, deck.cardValuesRemaining); // draws 2 from cardsRemaining
+      bot = new Bot(deck.cardsRemaining); // draws 2 from cardsRemaining
     }
 
     for (Gambler bot : totalBots){
@@ -42,9 +50,27 @@ public class Woo{
     totalPlayers.add(humanPlayer);
 
     System.out.println("Remaining cards in the deck: " + deck.cardsRemaining); // diag
-    System.out.println("Corresponding values: \t" + deck.cardValuesRemaining); // diag
     System.out.println("Remaining # of cards: " + deck.cardsRemaining.size()); // diag
 
+    for (int i = 0; i < totalPlayers.size(); i++){
+      Gambler gambler = totalPlayers.get(i);
+      int playerHand = gambler.getInHand();
 
+      while (true){
+        gambler.aceCheck();
+        if (playerHand == 21){
+          System.out.println("Blackjack!");
+          break;
+        }
+        else if (playerHand > 21){
+          System.out.println("Bust!");
+          break;
+        }
+        if (gambler.nextMove() == true){
+          break;
+        }
+      }
+
+    }
   }
 }
